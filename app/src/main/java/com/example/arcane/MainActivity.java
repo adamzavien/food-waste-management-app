c41cb7904ab0dd755fcfcc1f399832df444ffc6b
adamzavien <adamzavien@gmail.com>
2022-12-29 14:13:26 +0800

    food-waste-management-app

--- app/src/main/java/com/example/arcane/MainActivity.java
+++ app/src/main/java/com/example/arcane/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.arcane;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,9 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Food Waste Management App");
 
         btn_signUp = findViewById(R.id.btn_signUp);
         btn_signIn = findViewById(R.id.btn_signIn);
+
+        // grab constraint layout and store it into the variable
+        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
+
+        // create animation drawable object
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
 
         btn_signUp.setOnClickListener(new View.OnClickListener() {
             @Override
